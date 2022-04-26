52702beecd66f22c2e3a44a74bb1446a67e6c2bb
Inaya Reddick <104162368+InayaReddick@users.noreply.github.com>
2022-04-26 18:21:16 +0000

    Loop works

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ userInput = (input("Are you thirsty and want a sweet drink?" + " Answer yes or n
 
 
 table = [
-  ["1. Caramel Milk Tea", "$5", "Lychee Iced Tea", "$4.75", "Double Boba", "$0.75"],
-  ["2. Oreo Milk Tea", "$5", "Honey Black Tea", "$4.25", "Jellies", "$0.50"],
-  ["3. Hokkaido Milk Tea", "$5", "Peach Strawberry Fruit Tea", "$4.75", "Popping Boba", "$0.50"]
+  ["1. Caramel Milk Tea", "$5", "4. Lychee Iced Tea", "$4.75", "7. Double Boba", "$0.75"],
+  ["2. Oreo Milk Tea", "$5", "5. Honey Black Tea", "$4.25", "8. Jellies", "$0.50"],
+  ["3. Hokkaido Milk Tea", "$5", "6. Peach Strawberry Fruit Tea", "$4.75", "9. Popping Boba", "$0.50"]
 ]
 
 headers = ["Milk Tea", " $", "Iced Tea", "  $", "Extras", " $"]
@@ -16,25 +16,17 @@ if userInput == "yes":
   print("Let me interest you in some boba!")
   print(tabulate(table, headers, tablefmt="fancy_grid"))
 
-if userInput == "no":
+elif userInput == "no":
   print(input("Oh okay. Have a good day!"))
 
 
 thirsty = (input("Would you like to buy any of these drinks?" + " Answer yes or no: "))
 
 while thirsty == "yes":
-  iWant = (input("List the drink you want here. :"))
+  iWant = (input(" Write the number of the drink/drinks you want here. Do not include extras here :"))
+
+list []
+
 while thirsty == "no":
-    break
-    print("Oki")
-
-
-
-  
-  
-
-
-
-
-
-    
+  print("Okay 😭 enjoy your day")
+  break 
